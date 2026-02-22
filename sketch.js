ad18e34a965c4717319b294a6ea5d8a09cc83c1e
Jorge Valdez Gamboa <jorge.valdez743ciop6@gmail.com>
2026-02-22 07:17:14 +0000

    modified:   sketch.js

--- sketch.js
+++ sketch.js
@@ -1,10 +1,6 @@
 function setup (){
-  let canvas = createCanvas(
-    Global.columns * Global.cellSize,
-    Global.rows * Global.cellSize
-  );
-
-  canvas.parent("canvas-container");
+  createCanvas(Global.columns * Global.cellSize, Global.rows * Global.cellSize)
+    .parent("canvas-container");
   
   document.querySelector('canvas').addEventListener('contextmenu', e => e.preventDefault())
   
